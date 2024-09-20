35e666f5d4716f00a86fb5a015ca013ed248864c
Prishton-k <prishtonkc60@gmail.com>
2024-09-20 14:14:55 -0600

    added square

--- interactive_scene/sketch.js
+++ interactive_scene/sketch.js
@@ -2,45 +2,14 @@
 // Digdarshan KC
 // September-21-2024
 
-
-
-
 let x = 200;
 let y = 200;
-let speed = 4;
-let luigiImg;
-
-function preload() {
-  luigiImg = loadImage("luigi2.png");
-}
 
 function setup() {
   createCanvas(400, 400);
+  square(x,y,50)
 }
 
 function draw() {
   background(220);
-  
-  moveCharacter();
-  showCharacter();
-}
-
-function moveCharacter() {
-  if (keyIsDown(87)) { //w
-    y -= speed;   // y = y - speed;
-  }
-  if (keyIsDown(83)) { //s
-    y += speed;   // y = y + speed;
-  }
-  if (keyIsDown(65)) { //a
-    x -= speed;   // x = x - speed;
-  }
-  if (keyIsDown(68)) { //d
-    x += speed;   // x = x + speed;
-  }
-}
-
-function showCharacter() {
-  // square(x, y, 50);
-  image(luigiImg, x, y, luigiImg.width * 0.2, luigiImg.height * 0.2);
 }
