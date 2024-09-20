5ee73fe91fcc8928e21c9266da1e021d0cd70df1
Prishton-k <prishtonkc60@gmail.com>
2024-09-20 14:04:15 -0600

    Update sketch.js

--- interactive_scene/sketch.js
+++ interactive_scene/sketch.js
@@ -1,15 +1,46 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Interactive Scene
+// Digdarshan KC
+// September-20-2024
 
+
+
+
+let x = 200;
+let y = 200;
+let speed = 4;
+let luigiImg;
+
+function preload() {
+  luigiImg = loadImage("luigi2.png");
+}
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
+  
+  moveCharacter();
+  showCharacter();
+}
+
+function moveCharacter() {
+  if (keyIsDown(87)) { //w
+    y -= speed;   // y = y - speed;
+  }
+  if (keyIsDown(83)) { //s
+    y += speed;   // y = y + speed;
+  }
+  if (keyIsDown(65)) { //a
+    x -= speed;   // x = x - speed;
+  }
+  if (keyIsDown(68)) { //d
+    x += speed;   // x = x + speed;
+  }
+}
+
+function showCharacter() {
+  // square(x, y, 50);
+  image(luigiImg, x, y, luigiImg.width * 0.2, luigiImg.height * 0.2);
 }
