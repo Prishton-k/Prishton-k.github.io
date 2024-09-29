61b9ad1d8517c01b409e1dd0bfd9e8214fea5f86
Prishton-k <prishtonkc60@gmail.com>
2024-09-28 18:24:00 -0600

    Update sketch.js

--- interactive_scene/sketch.js
+++ interactive_scene/sketch.js
@@ -1,16 +1,41 @@
-// Interactive Scene
-// Digdarshan KC
-// September-21-2024
-
-let x = 200;
-let y = 200;
+// State variables
+let circleSize = 50;
+let circleColor = 0;
 
 function setup() {
   createCanvas(400, 400);
+  background(255);
 }
 
 function draw() {
-  background(220);
-  fill("blue")
-  square(mouseX,mouseY,10);
+  background(255);
+  drawCircle();
+  handleKeyboardInput();
+}
+
+function drawCircle() {
+  // Circle follows the mouse
+  fill(circleColor);
+  ellipse(mouseX, mouseY, circleSize, circleSize);
+}
+
+function handleKeyboardInput() {
+  // Change circle size based on keyboard input
+  if (keyIsDown(UP_ARROW)) {
+    circleSize += 1;
+  } else if (keyIsDown(DOWN_ARROW)) {
+    circleSize -= 1;
+  }
+  
+  // Change circle color based on keyboard input
+  if (keyIsDown('r')) {
+    circleColor = 255;
+  } else if (keyIsDown('b')) {
+    circleColor = 0;
+  }
+}
+
+function mouseWheel(event) {
+  // Optional: handle mouse wheel input (Extra for Experts)
+  // circleSize += event.delta;
 }
