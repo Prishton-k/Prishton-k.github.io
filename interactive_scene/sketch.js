fa00f541ab4fa3ef5e4ae05a102bb8a5b9e884b3
Prishton-k <prishtonkc60@gmail.com>
2024-10-01 22:41:48 -0600

    Update sketch.js

--- interactive_scene/sketch.js
+++ interactive_scene/sketch.js
@@ -14,6 +14,7 @@ function setup() {
   background(255);
 }
 
+//settig the background and function
 function draw() {
   background(170);
   drawShape();
@@ -22,31 +23,31 @@ function draw() {
 
 function drawShape() {
   fill(circleColor);
-  if (shape === 'circle') {
-    ellipse(mouseX, mouseY, 50, 50);
+  if (shape === 'circle') {//make a circle
+    ellipse(mouseX, mouseY, 50, 50);//size of circle and move the object where the mosue is
   } else if (shape === 'rect') {
-    rect(mouseX - circleSize*2, mouseY - circleSize, circleSize, circleSize);
+    rect(mouseX - circleSize*2, mouseY - circleSize, circleSize, circleSize);//move the object where the mouse is and size of the object 
   }
 }
 
-function handleKeyboardInput() {
-  if (keyIsDown(UP_ARROW)) {
+function handleKeyboardInput() {//change the size of object 
+  if (keyIsDown(UP_ARROW)) {//when click the up arrow increase the size of object by 5
     circleSize += 5;
-  } else if (keyIsDown(DOWN_ARROW)) {
+  } else if (keyIsDown(DOWN_ARROW)) {//when click the down arrow decrease the size of object by 5
     circleSize -= 5;
   }
   
-  if (key === 'c') {
+  if (key === 'c') {//change the object colour randomly in  any colour
     noStroke();
     circleColor = color(random(255), random(255), random(255));
   }
   
   
   } 
-  if (key === 'Arrowleft'); {
+  if (key === 'Arrowleft'); {//When click the left arrow change the circle into square
     shape = 'rect';
   }
-  if (key === 'ArrowRight'){
+  if (key === 'ArrowRight'){// If clicks the right arrow change the object back to circle
     shape = 'circle';
   }
   
