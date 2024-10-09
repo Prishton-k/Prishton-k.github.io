44aa7dcfb52f3ea112817158563c5e5bd824b5b7
Prishton-k <prishtonkc60@gmail.com>
2024-10-09 14:13:23 -0600

    array project

--- array_notation/sketch.js
+++ array_notation/sketch.js
@@ -3,13 +3,22 @@
 // Date
 //
 // Extra for Experts:
-
+let x = random(0, 400);
+let y = random(0,400);
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
+function preload() {
+  sky = loadImage("sky.png");
+}
 
 function draw() {
   background(220);
-  circle(mouseX, mouseY,100,100);
+  fill(220);
+  circle(800,400,100,100);
+  line( 800,0, 800, 780);
+  image(sky);
 }
+
+
