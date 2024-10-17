55cbf67b19d1ccf2862f094b33eb40c2870d6899
Prishton-k <prishtonkc60@gmail.com>
2024-10-16 22:50:23 -0600

    Update sketch.js

--- array_notation/sketch.js
+++ array_notation/sketch.js
@@ -3,20 +3,21 @@
 // Date
 //
 // Extra for Experts:
+ 
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(255);
+    createCanvas(windowWidth, windowHeight); 
 }
-
-function draw() {
-  background(225);
-  fill(255);
-  circle(800,370,200);
-  line( 800,0, 800, 780);
-}
-
-
-
+  function draw() {
+    // Generate random RGB values
+    let r = random(255);
+    let g = random(255);
+    let b = random(255);
+    // Set the background color
+    background(r,g,b);
+    circle(800,370,200);
+    line( 800,0, 800, 780);
+ 
+  }
 
 
