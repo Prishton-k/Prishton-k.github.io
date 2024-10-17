8913b2cf70919b1ff6c3fa23f5f5612aefea4549
Prishton-k <prishtonkc60@gmail.com>
2024-10-17 13:48:44 -0600

    rotate

--- array_notation/sketch.js
+++ array_notation/sketch.js
@@ -6,18 +6,25 @@
  
 
 function setup() {
-    createCanvas(windowWidth, windowHeight); 
+  createCanvas(windowWidth, windowHeight); 
 }
-  function draw() {
-    // Generate random RGB values
-    let r = random(255);
-    let g = random(255);
-    let b = random(255);
-    // Set the background color
-    background(r,g,b);
-    circle(800,370,200);
-    line( 800,0, 800, 780);
+function draw() {
+
+  // Generate random RGB values
+  let r = random(255);
+  let g = random(255);
+  let b = random(255);
+  // Set the background color
+  background(r,g,b);
+  circle(windowWidth/2,windowHeight/2,200);
+  line( windowWidth/2,0, windowWidth/2, windowHeight);
  
-  }
+}
+
+//making a paddle
+function draw_paddle(){
+  rect(windowWidth,windowHeight, 50,50);
+  
+}
 
 
