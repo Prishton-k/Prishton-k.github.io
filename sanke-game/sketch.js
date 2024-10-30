27b6eebd83400445b6121e1346450f6cc26cf9eb
Prishton-k <prishtonkc60@gmail.com>
2024-10-30 13:24:26 -0600

    just getting started

--- sanke-game/sketch.js
+++ sanke-game/sketch.js
@@ -1,10 +1,9 @@
-// Project Title
-// Your Name
-// Date
+// Snake game
+// Digdarshan KC
+// 30th Oct 2024
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -12,4 +11,11 @@ function setup() {
 
 function draw() {
   background(220);
+  snake();
 }
+
+function snake(){
+  square(50,50,50);
+}
+
+if 
